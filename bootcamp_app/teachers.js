46a8923baddc2615a17fd466ef7fc4a769e218db
vagrant <ttjho@hotmail.com>
2020-10-15 04:02:00 +0000

    santized process.argv user input for teachers.js for cohort names

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,18 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const arguments = process.argv.slice(2);
-
+// const arguments = process.argv.slice(2);
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`]
 pool.query(`
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM teachers
 JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
 JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name = '${arguments[0]}'
+WHERE cohorts.name LIKE $1
 ORDER BY teachers.name
 
-`)
+`,values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort} : ${user.teacher}`)
